f2e5686c58f9a0869a11e6b796afddc3a688c702
joy-mahmud <joymahmudiucse@gmail.com>
2025-11-08 22:52:20 +0600

    gallary section with light box

--- src/components/Home/Gallary.jsx
+++ src/components/Home/Gallary.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import { MdOutlineCameraAlt } from 'react-icons/md'
 import gradutionImg1 from '../../assets/gallary/graduation1.jpg'
 import gradutionImg2 from '../../assets/gallary/graduation2.jpg'
 import gradutionImg3 from '../../assets/gallary/graduation3.jpg'
 import gradutionImg4 from '../../assets/gallary/graduation4.jpg'
+import { Navigation, Autoplay } from 'swiper/modules';
+import Lightbox from 'yet-another-react-lightbox';
+import "yet-another-react-lightbox/styles.css";
 const Gallary = () => {
+    const [openLightBox, setOpenLightBox] = useState(false)
+    const [photoIndex, setPhotoIndex] = useState(0)
     const gallaryImg = [
+        { title: "graduation image 1", img: gradutionImg1 },
+        { title: "graduation image 2", img: gradutionImg2 },
+        { title: "graduation image 3", img: gradutionImg3 },
+        { title: "graduation image 4", img: gradutionImg4 },
         { title: "graduation image 1", img: gradutionImg1 },
         { title: "graduation image 2", img: gradutionImg2 },
         { title: "graduation image 3", img: gradutionImg3 },
@@ -26,20 +37,51 @@ const Gallary = () => {
 
                 {/* Right arrows */}
                 <div className='flex gap-3 items-center'>
-                    <IoIosArrowBack className='cursor-pointer text-2xl' />
-                    <IoIosArrowForward className='cursor-pointer text-2xl' />
+                    <IoIosArrowBack className='swiper-button-prev cursor-pointer text-2xl' />
+                    <IoIosArrowForward className='swiper-button-next cursor-pointer text-2xl' />
                 </div>
             </div>
 
             <div className='border border-t-0 border-gray-300 rounded-b-xl px-3 py-3 h-[250px]'>
-                <div className='grid grid-cols-4 gap-2'>
+                <Swiper
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    loop={true}
+                    modules={[Navigation, Autoplay]}
+                    autoplay={{
+                        delay: 1500,
+                        disableOnInteraction: false,
+                        pauseOnMouseEnter: true
+                    }}
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev'
+                    }}
+                    className='mySwiper'
+                    grabCursor={true}
+                    freeMode={true}
+
+                >
+
                     {
                         gallaryImg.map((item, id) => (
-                            <img key={id} src={item.img} className='h-[225px]' />
+                            <SwiperSlide key={id}>
+                                <img onClick={() => {
+                                    setOpenLightBox(true)
+                                    setPhotoIndex(id)
+                                }} src={item.img} alt={item.title} className='h-[225px] rounded-lg hover:scale-105 duration-300 transition-transform' />
+                            </SwiperSlide>
                         ))
                     }
-                </div>
 
+
+                </Swiper>
+                <Lightbox
+                    open={openLightBox}
+                    close={() => setOpenLightBox(false)}
+                    index={photoIndex}
+                    slides={gallaryImg.map((item) => ({ src: item.img }))}
+                />
             </div>
         </div>
 
